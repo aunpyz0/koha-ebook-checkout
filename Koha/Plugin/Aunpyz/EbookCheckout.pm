31a7978f5101b97c3da424a2a3ea1d8ea2aaf7be
Aunpyz <aunnop_kattiyanet@hotmail.com>
2025-04-14 20:06:15 +0700

    Execute sql to show barcode in OPAC

--- Koha/Plugin/Aunpyz/EbookCheckout.pm
+++ Koha/Plugin/Aunpyz/EbookCheckout.pm
@@ -63,14 +63,16 @@ sub install() {
 
     # TODO: customize marc_tag_structure to have tag 857
     # TODO: customize marc_subfield_structure to have tagsubfield u for tagfield 857
-    # TODO: execute these sql
-    # INSERT INTO koha_cts.columns_settings (module,page,tablename,columnname,cannot_be_toggled,is_hidden) VALUES
-    #  ('opac','biblio-detail','holdingst','item_barcode',0,0) ON DUPLICATE KEY UPDATE is_hidden=0, cannot_be_toggled=0;
-    # https://git.koha-community.org/Koha-community/Koha/src/branch/17.11.x/admin/columns_settings.pl
-    # INSERT INTO koha_cts.borrower_attribute_types (code,description,`repeatable`,unique_id,opac_display,opac_editable,staff_searchable,authorised_value_category,display_checkout,category_code,class) VALUES
-    #  ('SHOW_BCODE','Show Barcode',0,0,0,0,0,'',0,NULL,'') ON DUPLICATE KEY UPDATE code=code;
-    # opac/opac-user.pl:289
-    # koha-tmpl/opac-tmpl/bootstrap/en/modules/opac-user.tt:282
+    my @installer_statements = (
+        q { INSERT INTO columns_settings (module,page,tablename,columnname,cannot_be_toggled,is_hidden) VALUES
+         ('opac','biblio-detail','holdingst','item_barcode',0,0) ON DUPLICATE KEY UPDATE is_hidden=0, cannot_be_toggled=0; },
+        q { INSERT INTO koha_cts.borrower_attribute_types (code,description,`repeatable`,unique_id,opac_display,opac_editable,staff_searchable,authorised_value_category,display_checkout,category_code,class) VALUES
+         ('SHOW_BCODE','Show Barcode',0,0,0,0,0,'',0,NULL,'') ON DUPLICATE KEY UPDATE code=code; },
+    );
+    for ( @installer_statements ) {
+        my $sth = C4::Context->dbh->prepare( $_ );
+        $sth->execute or die C4::Context->dbh->errstr;
+    }
 
     my $opacuserjs = $self->_prepareopacuserjs();
 
